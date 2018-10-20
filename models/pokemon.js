6ea9bc65ab4a1ff85eac0bb50c7d29b02b52d685
Eva Panizo <panizo.eva@gmail.com>
2018-10-20 17:43:07 +0200

    Add pokemon model + Add database seed

--- models/pokemon.js
+++ models/pokemon.js
@@ -3,6 +3,15 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 // Definition of the Schema
+const pokemonSchema = new Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  image: {
+    type: String
+  }
+});
 
 // Definition of the Model
 const Pokemon = mongoose.model('Pokemon', pokemonSchema);
