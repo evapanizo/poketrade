2c6538b2e03c9368cf30f947d24ea1ef62cb21d0
Alexandra Pedroche <alexandrapedroche@gmail.com>
2018-10-21 17:05:13 +0200

    Add middlewares, index view, images, auth views

--- middlewares/middlewares.js
+++ middlewares/middlewares.js
@@ -1,3 +1,49 @@
+<<<<<<< HEAD
+//no tengo el model de users
+const User = require('../models/users');
+//comprobar que los campos de user y pass no estan vacios
+function emptyFields (req, res, next) {
+  const { username, password } = req.body;
+  if (!username || !password) {
+    res.render('users/signup', { error: 'The fields should not be empty' }) 
+  } else {
+    next();
+  } 
+}
+//comprobar que el user esta logeado
+function isLogged (req, res, next) {
+  const user = req.session.currentUser;
+  if (!user) {
+    res.redirect('/users/login');
+  } else { next(); }
+} 
+
+function isAnon (req, res, next) { 
+  const user = req.session.currentUser;
+  if (user) {
+    res.redirect('/users/profile');
+  } else { next(); } 
+}
+//comprobar que el user no esta usado ya
+function isCreated (req, res, next) {
+  const { username } = req.body;
+  User.findOne({ username })
+    .then((user) => {
+      if (user) {
+        res.render('users/signup',  { error: 'The user already exists, choose another name' }),  
+    } else {
+      next();
+    }
+  })
+  .catch(next)
+}
+ module.exports= {
+  emptyFields,
+  isLogged,
+  isAnon,
+  isCreated
+} 
+=======
 'use strict';
 
 function requireUser (req, res, next) {
@@ -13,3 +59,4 @@ function requireUser (req, res, next) {
 module.exports = {
   requireUser
 };
+>>>>>>> 62378e9e78ad8eb56f5c60727384ddffa3c68308
